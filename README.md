9d621eec7bc64bae63ae8a74e3cc346ea7444704
Robert Starsi <klevo@klevo.sk>
2024-09-02 13:30:01 +0200

    ruby -Ilib/ bin/enviroblyd metadata

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Envirobly instance daemon
 
 ```sh
 ruby -Ilib/ bin/enviroblyd version
+
+export ENVIROBLYD_IMDS_HOST=envirobly.test
+ruby -Ilib/ bin/enviroblyd metadata
 ```
--- lib/enviroblyd/cli/main.rb
+++ lib/enviroblyd/cli/main.rb
@@ -5,4 +5,14 @@ class Enviroblyd::Cli::Main < Enviroblyd::Base
   def version
     puts Enviroblyd::VERSION
   end
+
+  TOKEN_TTL_SECONDS = 30
+  IMDS_HOST = ENV.fetch("ENVIROBLYD_IMDS_HOST", "[fd00:ec2::254]")
+  desc "metadata", "Get IMDSv2 metadata"
+  def metadata
+    token = `curl -s -X PUT "http://#{IMDS_HOST}/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: #{TOKEN_TTL_SECONDS}"`
+    puts "token: #{token}"
+    instance_id = `curl -s -H "X-aws-ec2-metadata-token: #{token}" http://#{IMDS_HOST}/latest/meta-data/instance-id`
+    puts "instance_id: #{instance_id}"
+  end
 end
