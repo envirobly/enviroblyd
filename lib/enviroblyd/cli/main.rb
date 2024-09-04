147d660f55c92337e108dd22cecc53dc6516addb
Robert Starsi <klevo@klevo.sk>
2024-09-04 10:20:29 +0200

    init to ENVIROBLYD_INIT_URL

--- lib/enviroblyd/cli/main.rb
+++ lib/enviroblyd/cli/main.rb
@@ -2,6 +2,8 @@
 require "net/http"
 require "uri"
 require "open3"
+require "fileutils"
+require "pathname"
 
 class Enviroblyd::Cli::Main < Enviroblyd::Base
   desc "version", "Show version"
@@ -12,19 +14,33 @@ class Enviroblyd::Cli::Main < Enviroblyd::Base
   TOKEN_TTL_SECONDS = 30
   IMDS_HOST = ENV.fetch("ENVIROBLYD_IMDS_HOST", "169.254.169.254")
   API_HOST = ENV.fetch("ENVIROBLYD_API_HOST", "envirobly.com")
-  desc "boot", "Get IMDSv2 metadata"
+  INIT_URL = ENV.fetch("ENVIROBLYD_INIT_URL")
+  WORKING_DIR = Pathname.new ENV.fetch("ENVIROBLYD_WORKING_DIR", "/var/envirobly/daemon")
+  INITIALIZED_FILE = WORKING_DIR.join "initialized"
+  desc "boot", "Start the daemon"
   def boot
-    @token = http("http://#{IMDS_HOST}/latest/api/token",
-      type: Net::HTTP::Put, headers: { "X-aws-ec2-metadata-token-ttl-seconds" => TOKEN_TTL_SECONDS.to_s }).
-      body.chomp("")
-    puts "token: #{@token} ."
-    instance_id = http("http://#{IMDS_HOST}/latest/meta-data/instance-id",
-      headers: { "X-aws-ec2-metadata-token" => @token }).
-      body.chomp("")
-    puts "instance_id: #{instance_id} ."
+    # @token = http("http://#{IMDS_HOST}/latest/api/token",
+    #   type: Net::HTTP::Put, headers: { "X-aws-ec2-metadata-token-ttl-seconds" => TOKEN_TTL_SECONDS.to_s }).
+    #   body.chomp("")
+    # puts "token: #{@token} ."
+    # instance_id = http("http://#{IMDS_HOST}/latest/meta-data/instance-id",
+    #   headers: { "X-aws-ec2-metadata-token" => @token }).
+    #   body.chomp("")
+    # puts "instance_id: #{instance_id} ."
+    #
+    # response = http("https://#{API_HOST}/api/v1/boots/#{instance_id}", retry_interval: 3, retries: 5, backoff: :exponential)
+    # puts "/api/v1/boots response code: #{response.code}"
 
-    response = http("https://#{API_HOST}/api/v1/boots/#{instance_id}", retry_interval: 3, retries: 5, backoff: :exponential)
-    puts "/api/v1/boots response code: #{response.code}"
+    if File.exist?(INITIALIZED_FILE)
+      puts "Skipping initialization because #{INITIALIZED_FILE} exists."
+    else
+      puts "Init URL: #{INIT_URL}"
+      response = http(INIT_URL, type: Net::HTTP::Put, retry_interval: 3, retries: 10, backoff: :exponential)
+      puts "Init response code: #{response.code}"
+
+      FileUtils.mkdir_p WORKING_DIR
+      File.write INITIALIZED_FILE, INIT_URL
+    end
   end
 
   private
@@ -35,7 +51,7 @@ class Enviroblyd::Cli::Main < Enviroblyd::Base
       http.open_timeout = 5
       http.read_timeout = 5
 
-      request = type.new(uri, headers)
+      request = type.new(uri, default_headers.merge(headers))
       # request.content_type = CONTENT_TYPE
 
       yield request if block_given?
@@ -70,5 +86,10 @@ class Enviroblyd::Cli::Main < Enviroblyd::Base
         @stderr = stderr.read
         @exit_code = thread.value.exitstatus
       end
+    end
+
+    USER_AGENT = "enviroblyd #{Enviroblyd::VERSION}"
+    def default_headers
+      { "User-Agent" => USER_AGENT }
     end
 end
