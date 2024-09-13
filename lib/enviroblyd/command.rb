c7d4ef03da9613d4ecda7e1e13935a90740a6737
Robert Starsi <klevo@klevo.sk>
2024-09-13 10:59:09 +0200

    Command does parsing and validation

--- lib/enviroblyd/command.rb
+++ lib/enviroblyd/command.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 require "open3"
+require "json"
 
 class Enviroblyd::Command
   DEFAULT_TIMEOUT_SECONDS = 5 * 60
   DEFAULT_RUNTIME = "/bin/bash"
 
-  def initialize(params)
+  def initialize(message)
+    params = parse_message message
     @url = params.fetch "url"
     @script = params.fetch "script"
     @runtime = params.fetch "runtime", DEFAULT_RUNTIME
     @timeout = params.fetch "timeout", DEFAULT_TIMEOUT_SECONDS
     @stdout = @stderr = @exit_code = nil
+    @valid = true
+  rescue NoMethodError, KeyError
+    @valid = false
+  end
+
+  def valid?
+    @valid
   end
 
   def run
@@ -38,5 +47,11 @@ class Enviroblyd::Command
           exit_code: @exit_code
         }
       }
+    end
+
+    def parse_message(message)
+      JSON.parse message
+    rescue
+      nil
     end
 end
--- lib/enviroblyd/daemon.rb
+++ lib/enviroblyd/daemon.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 require "socket"
-require "json"
 
 class Enviroblyd::Daemon
   MAX_MESSAGE_SIZE = 6000 # bytes
-  LISTEN_PORT = ENV.fetch("ENVIROBLYD_PORT", 63106).to_i
+  PORT = ENV.fetch("ENVIROBLYD_PORT", 63106).to_i
 
   def self.start
     imds = Enviroblyd::IMDS.new
@@ -19,36 +18,27 @@ class Enviroblyd::Daemon
   end
 
   def listen
-    server = TCPServer.new @host, LISTEN_PORT
-    puts "Listening on #{@host}:#{LISTEN_PORT}"
+    server = TCPServer.new @host, PORT
+    puts "Listening on #{@host}:#{PORT}"
     Enviroblyd::Web.register
 
     loop do
       Thread.start(server.accept) do |client|
-        params = parse_message client.recv(MAX_MESSAGE_SIZE)
+        command = Enviroblyd::Command.new client.recv(MAX_MESSAGE_SIZE)
 
-        if params.nil?
+        unless command.valid?
           client.puts "Invalid message"
           next
         end
 
         client.puts "OK"
         client.close
-
-        command = Enviroblyd::Command.new(params)
         command.run
       ensure
         client.close
         command = nil
-        params = nil
         GC.start
       end
     end
-  end
-
-  def parse_message(message)
-    JSON.parse message
-  rescue
-    nil
   end
 end
